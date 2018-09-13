b7edecf49d80548bd64ad4e68a3558471864c947
xuanngo90 <ngoducxuan1990@gmail.com>
2018-09-13 13:21:16 +0700

    Change GulpFile

--- gulpfile.js
+++ gulpfile.js
@@ -8,34 +8,62 @@ var sourcemaps = require('gulp-sourcemaps');
 // Reload Browser when save
 var browserSync = require('browser-sync').create();
 
-gulp.task('serve', ['css', 'html'], function() {
+gulp.task('serve', ['sass', 'html', 'css'], function() {
 
   browserSync.init({
       server: "./build"
   });
-
-  gulp.watch("client/templates/*.scss", ['css']);
+ 
+  gulp.watch('client/templates/**/*.css', ['css']);
+  gulp.watch("client/templates/**/*.scss", ['sass']);
   gulp.watch("client/templates/*.jade", ['html']);
   gulp.watch("build/*.html").on('change', browserSync.reload);
 });
 
-// 
+// End Reload Browser 
 
 gulp.task('html', function(){
-  return gulp.src('client/templates/*.jade')
+  var YOUR_LOCALS = {};
+
+  return gulp.src('client/templates/**/*.jade')
     .pipe(jade())
     .pipe(gulp.dest('build/'))
     .pipe(browserSync.stream());
 });
 
-gulp.task('css', function(){
-  return gulp.src('client/templates/*.scss')
+gulp.task('sass', function(){
+  return gulp.src('client/templates/**/*.scss')
     .pipe(sass())
     .on('error', function (error) {
       console.log(error.toString());
     })
     .pipe(minifyCSS())
     .pipe(gulp.dest('build/css'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('css', function(){
+  return gulp.src('client/templates/**/*.css')
+    .on('error', function (error) {
+      console.log(error.toString());
+    })
+    .pipe(minifyCSS())
+    .pipe(gulp.dest('build/css'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('img', function(){
+  return gulp.src('client/templates/images/*')
+    .pipe(gulp.dest('build/images'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('jsLib', function(){
+  return gulp.src('client/javascript/libs/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(concat('libs.min.js'))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('build/js'))
     .pipe(browserSync.stream());
 });
 
@@ -50,9 +78,12 @@ gulp.task('js', function(){
 
 
 gulp.task('watch',  () => {
-  gulp.watch('client/templates/*.scss', ['css']);
-  gulp.watch('client/templates/*.jade', ['html']);
+  gulp.watch('client/templates/images/*', ['img']);
+  gulp.watch('client/templates/**/*.css', ['css']);
+  gulp.watch('client/templates/**/*.scss', ['sass']);
+  gulp.watch('client/templates/**/*.jade', ['html']);
   gulp.watch('client/javascript/*.js', ['js']);
+  gulp.watch('client/javascript/libs/*.js', ['jsLib']);
 });
 
-gulp.task('default', ['serve', 'html', 'css', 'js', 'watch' ]);
+gulp.task('default', ['serve', 'html', 'sass', 'js', 'jsLib', 'img', 'watch' ]);
